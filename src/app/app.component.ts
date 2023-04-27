607983b28d3c669c1b5b5594265714ed8694dfd5
Hrushikesh61 <hrushikeshpatil61@gmail.com>
2023-04-27 18:11:47 +0530

    first commit.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Calculator';
+  toshow = '0';
+  data=this.toshow;
+  writeToInput(value:string){
+    if(this.toshow =='0')this.toshow = '';
+    this.toshow = this.toshow + value;
+  }
+  addSign(value:string){
+    var lastchar = this.toshow.charAt(this.toshow.length -1);
+    if(lastchar === '-' || lastchar === '+' || lastchar === '/' ||lastchar === '*'){
+      this.toshow = this.toshow.substring(0,this.toshow.length-1);
+    }
+    this.toshow +=value;
+  }
+  equal(){
+    this.toshow = eval(this.toshow);
+  }
+  clearLast(){
+    if(this.toshow.length>0 && this.toshow!=='0'){
+        this.toshow = this.toshow.substring(0,this.toshow.length-1);
+        console.log(this.toshow);
+    }
+  }
+  clearall(){
+    this.toshow = "0";
+  }
 }
